995dbc5bf798346d8cf42bd87c8ae58a52b7faf8
Shri Om Trivedi <tshriom@gmail.com>
2023-08-29 08:54:20 +0530

    linear search added

--- recursion/arrays/linearSearch.py
+++ recursion/arrays/linearSearch.py
@@ -1,1 +1,20 @@
-class Search
+class Search():
+    def __init__(self) -> None:
+        pass
+
+    def linear_search(self, arr, target, idx):
+        # base condition
+        if idx == len(arr):
+            return -1
+        
+        if arr[idx] == target:
+            print({idx: idx, ele: arr[idx]})
+            return idx
+        else:
+            return self.linear_search(arr, idx + 1, target)
+        
+arr = [1, 4, 3, 2, 8, 9]
+target = 2
+some = Search()
+out = some.linear_search(arr, target, 0)
+print(out)
